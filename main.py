ed015bae370fa82167cf8d2620dd8d378651957d
Andraaz <guillaume.viney46@gmail.com>
2023-01-10 11:44:00 +0100

    Add Python update

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-Copy code
 import unittest
 
 class SimpleMath:
@@ -6,10 +5,18 @@ class SimpleMath:
     def addition(x, y):
         return x + y
 
+@staticmethod
+def subtraction(x, y):
+    return x - y
+
 class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
         result = SimpleMath.addition(5, 3)
         self.assertEqual(result, 8)
+
+    def test_subtraction(self):
+        result = SimpleMath.subtraction(5, 3)
+        self.assertEqual(result, 2)
 
 if __name__ == '__main__':
     unittest.main()
